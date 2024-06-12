1fffee7143a9d5f2c8fb2cec59d7416fa9a577a7
rachaeltochukwu <tochukwuthelma2022@gmail.com>
2024-06-12 05:14:39 -0700

    index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+
+
+score = 55
+if (score > 75) and (score <= 100):
+    print ("A")
+elif (score >= 60) and score < 75:
+    print("B")
+elif (score >= 50) and (score < 60):
+    print("C")
+elif (score >= 45) and (score < 50):
+     print("D")
+else:
+    print("F")
